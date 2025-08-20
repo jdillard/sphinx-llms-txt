5e63c8a3e6464ff31ceea35522c4b2553450b8a9
Jared Dillard <jared.dillard@gmail.com>
2025-08-19 20:55:56 -0700

    bump version

--- sphinx_llms_txt/__init__.py
+++ sphinx_llms_txt/__init__.py
@@ -21,7 +21,7 @@ from .manager import LLMSFullManager
 from .processor import DocumentProcessor
 from .writer import FileWriter
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 # Export classes needed by tests
 __all__ = [
