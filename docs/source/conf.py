886c6f48ba8a068b57c48fba605674647884bfe1
Jared Dillard <jared.dillard@gmail.com>
2025-10-26 22:12:51 -0700

    configure llms_txt_uri_template for markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import subprocess
 project = "sphinx-llms-txt"
 copyright = "Jared Dillard"
 author = "Jared Dillard"
+llms_txt_uri_template = "{base_url}{docname}.md"
 llms_txt_code_files = ["+:../../sphinx_llms_txt/*.py"]
 llms_txt_summary = """
 A Sphinx extension that generates a summary llms.txt file,written in Markdown,
