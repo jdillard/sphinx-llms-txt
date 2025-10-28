98c45ba0928ade85637e2eb4225b2082934ab758
Jared Dillard <jared.dillard@gmail.com>
2025-10-28 12:40:13 -0700

    move function to cmake folder

--- cmake/SetupSphinx.cmake
+++ cmake/SetupSphinx.cmake
@@ -1,5 +1,8 @@
 # SetupSphinx.cmake - Find Sphinx for CI environments
 
+set(SPHINX_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/source)
+set(SPHINX_BUILD ${CMAKE_BINARY_DIR})
+
 # Function to find Sphinx in the system
 function(setup_sphinx_environment)
     # Find sphinx-build executable in system
@@ -14,4 +17,12 @@ function(setup_sphinx_environment)
 
     # Export to parent scope
     set(SPHINX_EXECUTABLE "${SPHINX_EXECUTABLE}" PARENT_SCOPE)
+endfunction()
+
+# Function to add a Sphinx builder target
+function(add_sphinx_builder builder_name)
+    add_custom_target(${builder_name}
+        COMMAND ${SPHINX_EXECUTABLE} -b ${builder_name} ${SPHINX_SOURCE} ${SPHINX_BUILD}/${builder_name}
+        VERBATIM
+    )
 endfunction()
